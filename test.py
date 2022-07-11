201ab59fa55577b17581259c7c64597a60e224a1
Hasan <hasanusask@gmail.com>
2022-07-11 23:44:27 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+from app import app
+
+def test1():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+
+def test2():
+    response = app.test_client().get("/base")
+    assert response.status_code == 200
